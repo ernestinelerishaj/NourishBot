0fb3750a4da17fb2b9247c525c3325e04738e046
ernestinelerishaj <lerishajohn@gmail.com>
2025-06-16 11:17:01 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+# This is a placeholder Python file.
+# NourishBot is a chatbot-based web project built using IBM Watson Assistant and Wix (no-code).
+# This file represents the logical structure and AI integration that would be done in code-based systems.
+# Tools involved: IBM Watson Assistant, Wix Website Builder, Design Thinking, Chatbot Flow Mapping.
